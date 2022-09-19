b861f7c1ea2b9382b8fb383fc02bedd69c48ecc4
dmytro-bieliei <dmbieliei@gmail.com>
2022-09-19 21:23:46 +0300

    add changed names of variables

--- assets/index.js
+++ assets/index.js
@@ -1,25 +1,27 @@
 // Создайте функцию, которая принимает неограниченное количество входящих данных, и возвращает количество строк, переданных ей в этом массиве.
 
 // const func = (...string) => string.length;
-const quantityOfLine = (...string) => string.filter((element) => typeof element === 'string').length;
+const quantityOfLines = (...string) => string.filter((element) => typeof element === 'string').length;
 
 // Создайте функцию продвинутого калькулятора. Он принимает знак, а  затем неограниченное количество чисел, над которыми проведет указанную операцию. 
 
-const calcFunc = function(operator, ...number){
+const calcFunc = function(operator, ...numbers){
   switch(operator){
     case '*' : {
-      return number.reduce((num1, num2) => num1 * num2);
+      return numbers.reduce((num1, num2) => num1 * num2);
     }
     case '/' : {
-      return number.reduce((num1, num2) => num1 / num2);
+      return numbers.reduce((num1, num2) => num1 / num2);
     }
     case '+' : {
-      return number.reduce((num1, num2) => num1 + num2);
+      return numbers.reduce((num1, num2) => num1 + num2);
     }
     case '-' : {
-      return number.reduce((num1, num2) => num1 - num2);
+      return numbers.reduce((num1, num2) => num1 - num2);
     }
   }
 }
 
 // Имея массив чисел, найдите в нем самое большое. Не используйте циклы или методы перебора массива.
+
+const largestNumber = function
